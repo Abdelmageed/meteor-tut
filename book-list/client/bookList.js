a98a98b8136f4f5ac628255aa586424900e165b7
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-02-27 11:25:27 +0200

    publish/subscribe; to limit clients access to db.

--- book-list/client/bookList.js
+++ book-list/client/bookList.js
@@ -1,14 +1,10 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 import { Books } from '../imports/api/books.js';
 import './main.html';
 
-//Template.bookList.onCreated(function helloOnCreated() {
-  // counter starts at 0
-//  this.counter = new ReactiveVar(0);
-//  this.books = new ReactiveVar([ 
-      
-//]);
+Template.bookList.onCreated(function bookListOnCreated() {
+  Meteor.subscribe('books');
+});
 
 Template.bookList.helpers({
   books() {
--- book-list/imports/api/books.js
+++ book-list/imports/api/books.js
@@ -1,3 +1,10 @@
 import { Mongo } from 'meteor/mongo';
 
 export const Books = new Mongo.Collection('books');
+
+if (Meteor.isServer){
+  Meteor.publish('books', function booksPublication() {
+    return Books.find({}, {limit: 3});
+  });
+}
+
