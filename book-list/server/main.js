5074f3c19ef02ea83d8303d3f47a5c1b3bc5868e
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-02-27 10:44:22 +0200

    Book List template. Reads all books from db and display.

--- book-list/server/main.js
+++ book-list/server/main.js
@@ -1,5 +1,13 @@
 import { Meteor } from 'meteor/meteor';
+import { Books } from '../imports/api/books.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  
+   if (!Books.findOne()) {
+        Books.insert({title: "To Kill a Mockingbird", author: "Harper Lee"});
+        Books.insert({title: "1984", author: "George Orwell"});
+        Books.insert({title: "The Lord of the Rings", author: "J. R. R. Tolkien"});
+        Books.insert({title: "The Catcher in the Rye", author: "J. D. Salinger"});
+        Books.insert({title: "The Great Gatsby", author: "F. Scott Fitzgerald"});
+    }
 });
